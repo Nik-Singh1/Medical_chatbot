27ffa43b97f3d21ab97b02cd586d490911aa0585
Nik-Singh1 <s.niteshsingh999@gmail.com>
2025-12-02 11:11:42 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Medical_chatbot',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Nitesh Singh',
+    author_email='s.niteshsingh999@gmail.com'
+)
